8f951c56d779579476b7a6ce27b3b320ceeb54c7
Solene <solene.patruno@smile.fr>
2019-07-25 16:52:47 +0200

    we are making some fix into organisation

--- ssh-loading/test-sshloading.sh
+++ ssh-loading/test-sshloading.sh
@@ -1,27 +1,5 @@
 #!/bin/bash
-if [ ! -S ~/.ssh/ssh_auth_sock ]; then
-eval `ssh-agent`
-ln -sf "$SSH_AUTH_SOCK" ~/.ssh/ssh_auth_sock
-for i in 1 2 3
- do 
-  for i in 1 2 3
-   do
-    echo "			TOC"
-    sleep 0.5
-   done
- echo ""
- echo "			~~~[ $USER ]~~~"
- sleep 0.5
- echo ""
- done
-echo "Please Sheldon go back into Big Bang Theory !"
-echo "I just need your password silly ..."
-sleep 1
-echo "- - -"
-fi
-export SSH_AUTH_SOCK=~/.ssh/ssh_auth_sock
-ssh-add -l > /dev/null || ssh-add
 
-# NEXT EPISODE
-#echo "Something like LOTH"
-#echo "YOU SHOULDN'T PASS"
+echo "[####] NEXT EPISODE [####]"
+echo "[####]		    [####]"
+echo "[####] Something inspired by LOTH"
