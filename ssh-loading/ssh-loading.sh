8a8f72e63c7d5adc800aeb0ff7ed49a78a91f6c0
Solene <solene.patruno@smile.fr>
2019-07-25 16:05:37 +0200

    fix sheldon's bugs

--- ssh-loading/ssh-loading.sh
+++ ssh-loading/ssh-loading.sh
@@ -1,20 +1,27 @@
-#SSH training
-
+#!/bin/bash
 if [ ! -S ~/.ssh/ssh_auth_sock ]; then
-  eval `ssh-agent`
-  ln -sf "$SSH_AUTH_SOCK" ~/.ssh/ssh_auth_sock
-  echo "One to rule them all"
-  sleep 3
-  echo "."
-  sleep 1
-  echo ". ."
-  sleep 1
-  echo ". . ."
-  sleep 1
-  echo ". . . ."
-  sleep 1
-  echo ". . . . ."
+eval `ssh-agent`
+ln -sf "$SSH_AUTH_SOCK" ~/.ssh/ssh_auth_sock
+for i in 1 2 3
+ do 
+  for i in 1 2 3
+   do
+    echo "			TOC"
+    sleep 0.5
+   done
+ echo ""
+ echo "			~~~[ $USER ]~~~"
+ sleep 0.5
+ echo ""
+ done
+echo "Please Sheldon go back into Big Bang Theory !"
+echo "I just need your password silly ..."
+sleep 1
+echo "- - -"
 fi
 export SSH_AUTH_SOCK=~/.ssh/ssh_auth_sock
 ssh-add -l > /dev/null || ssh-add
 
+# NEXT EPISODE
+#echo "Something like LOTH"
+#echo "YOU SHOULDN'T PASS"
